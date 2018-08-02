75eeef6496037fafe88043736379369d1960dedd
Paweł <pawel.panek81@gmail.com>
2018-08-02 15:42:23 +0200

    02-08-2018 added authentication

---
+++ zuul-server/src/main/java/pl/mycar/zuulserver/config/JwtConfig.java
@@ -0,0 +1,24 @@
+package pl.mycar.zuulserver.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+class JwtConfig {
+  @Value("${security.jwt.uri:/auth/**}")
+  private String Uri;
+
+  @Value("${security.jwt.header:Authorization}")
+  private String header;
+
+  @Value("${security.jwt.prefix:Bearer }")
+  private String prefix;
+
+  @Value("${security.jwt.expiration:#{24*60*60}}")
+  private int expiration;
+
+  @Value("${security.jwt.secret:JwtSecretKey}")
+  private String secret;
+}
