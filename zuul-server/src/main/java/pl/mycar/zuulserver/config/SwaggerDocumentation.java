a42ea3822bbc190f71d21a034cefb8aeede3d201
Paweł P <panczo12d@gmail.com>
2018-11-17 18:27:36 +0100

    improve docs in zuul, added dev property to turbine

--- zuul-server/src/main/java/pl/mycar/zuulserver/config/SwaggerDocumentation.java
+++ zuul-server/src/main/java/pl/mycar/zuulserver/config/SwaggerDocumentation.java
@@ -17,9 +17,13 @@ public class SwaggerDocumentation implements SwaggerResourcesProvider {
   @Override
   public List<SwaggerResource> get() {
     List<SwaggerResource> resources = new ArrayList<>();
-    resources.add(swaggerResource("map-service", "/api/map/v2/api-docs", "2.0"));
     resources.add(swaggerResource("account-service", "/api/account/v2/api-docs", "2.0"));
-    resources.add(swaggerResource("car-manage-service", "/api/car/v2/api-docs", "2.0"));
+    resources.add(swaggerResource("map-service", "/api/map/v2/api-docs", "2.0"));
+    resources.add(swaggerResource("car-service", "/api/car/v2/api-docs", "2.0"));
+    resources.add(swaggerResource("examination-service", "/api/examination/v2/api-docs", "2.0"));
+    resources.add(swaggerResource("insurance-service", "/api/insurance/v2/api-docs", "2.0"));
+    resources.add(swaggerResource("notification-service", "/api/notification/v2/api-docs", "2.0"));
+
     return resources;
   }
 
