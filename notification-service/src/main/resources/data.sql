14622d9d3ccd88f6c3caeda96479f41ca2942ff7
pawel <panczo12d@gmail.com>
2018-11-18 22:02:14 +0100

    Changed initial data to more sophisticated

--- notification-service/src/main/resources/data.sql
+++ notification-service/src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into notifications(id, added_at, header, text, username, car_id, read)
-  values (1, '2018-10-06', 'Sprawdź ciśnienie w kołach', null, 'pawel96', 1, false);
+  values (1, '2018-12-05', 'Uważaj, od poniedziałku prognozowany śnieg', null, 'pawel96', 1, true);
 
 insert into notifications(id, added_at, header, text, username, car_id, read)
-  values (2, '2018-10-05', 'Uważaj, od poniedziałku prognozowany śnieg', null, 'pawel96', 1, true);
+  values (2, '2018-11-06', 'Sprawdź zewnętrzne oświetlenie pojazdu', null, 'pawel96', 1, false);
 
 insert into notifications(id, added_at, header, text, username, car_id, read)
-  values (3, '2018-10-04', 'Kończy się okres ważności badania technicznego', null, 'pawel96', 1, true);
+  values (3, '2018-10-16', 'Kończy się okres ważności badania technicznego', null, 'pawel96', 1, true);
 
 insert into notifications(id, added_at, header, text, username, car_id, read)
   values (4, '2018-10-03', 'Sprawdź głębokość bieżnika w oponach', null, 'pawel96', 1, false);
@@ -20,4 +20,4 @@ insert into notifications(id, added_at, header, text, username, car_id, read)
   values (7, '2018-09-30', 'Sprawdź poziom oleju', null, 'pawel96', 1, false);
 
 insert into notifications(id, added_at, header, text, username, car_id, read)
-  values (8, '2018-10-29', 'Skontroluj ciśnienie w oponach', null, 'pawel96', 1, false);
+  values (8, '2018-09-20', 'Skontroluj ciśnienie w oponach', null, 'pawel96', 1, false);
